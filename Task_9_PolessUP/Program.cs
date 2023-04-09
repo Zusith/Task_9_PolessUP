3df8b8d63173d20828df29194f55f260bccc99f4
Kartech <kartech222@outlook.com>
2023-04-09 23:11:38 +0300

    Добавьте файлы проекта.

---
+++ Task_9_PolessUP/Program.cs
@@ -0,0 +1,16 @@
+﻿int[] years = {44, 2015 };
+Console.WriteLine("Ввод:\t" + string.Join("\t", years));
+Console.Write("Вывод:");
+foreach (var year in years)
+{
+    Console.Write("\t" + LeapYearCheck(year));
+}
+
+bool LeapYearCheck(int year)
+{
+    if (year % 4 != 0)
+    return false;
+    else if (year % 100 != 0)  return true;  
+    else if (year % 400 == 0)  return true; 
+    else return false; 
+}
